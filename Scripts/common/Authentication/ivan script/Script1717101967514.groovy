584deb791052d7780c7174a91aef2fb6a2a7dae6
Ikramgh <ikramgharsallah1@gmail.com>
2024-06-02 14:08:50 +0200

    chnage failure handling for login auto

--- Scripts/common/Authentication/ivan script/Script1717101967514.groovy
+++ Scripts/common/Authentication/ivan script/Script1717101967514.groovy
@@ -18,18 +18,22 @@ import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
 WebUI.openBrowser('')
+
 try {
-	WebUI.authenticate('http://crm-dev1.objectway.it/OFSCRMWealthQA/main.aspx#717330153', 'crm.test1', 'Welcome1!', 30)
+    WebUI.authenticate('http://crm-dev1.objectway.it/OFSCRMWealthQA/main.aspx#717330153', 'crm.test1', 'Welcome1!', 30, 
+        FailureHandling.OPTIONAL)
 }
-catch (Exception) {
-	//do nothing
-}
- 
- 
+catch (def Exception) {
+    //do nothing
+} 
+
 def locatorSearchField = findTestObject('Page_Dashboards Customer Service Representa_7a7d3c/span_Dashboards_navImageFlipHorizontal')
-WebUI.waitForElementPresent(locatorSearchField, 60) //1 minute for login
+
+WebUI.waitForElementPresent(locatorSearchField, 60 //1 minute for login
+    )
+
 WebUI.click(locatorSearchField)
- 
+
 //WebUI.setText(findTestObject('Page_Dashboards Customer Service Representa_7a7d3c/input_Search Dynamics 365 data_search'), '004655')
- 
 WebUI.click(findTestObject('Page_Dashboards Customer Service Representa_7a7d3c/img_Search Dynamics 365 data_findCriteriaImg'))
+
